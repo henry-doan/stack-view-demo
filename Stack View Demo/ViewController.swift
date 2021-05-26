26c03d6ff9101fccafa71f85cca6e354ed613539
Henry Doan <henrydoan96@gmail.com>
2021-05-26 10:59:01 -0600

    stack view working

---
+++ Stack View Demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Stack View Demo
+//
+//  Created by Henry Doan on 5/26/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
